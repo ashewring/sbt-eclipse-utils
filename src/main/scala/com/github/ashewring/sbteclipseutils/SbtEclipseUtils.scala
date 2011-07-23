a22a2be0f0ad82b9a488010b07d0a15c500ebf66
ashewring <ashewring@example.com>
2011-07-23 18:35:34 +0200

    XML is now pretty printed instead of relying on indentation of the source file

--- src/main/scala/com/github/ashewring/sbteclipseutils/SbtEclipseUtils.scala
+++ src/main/scala/com/github/ashewring/sbteclipseutils/SbtEclipseUtils.scala
@@ -10,22 +10,29 @@ import java.util.jar.Manifest
 import collection.mutable.ListBuffer
 import java.io.{FileInputStream, File}
 
+object SbtEclipseUtils {
+	val PrettyPrinter = new PrettyPrinter(120, 2)
+}
+
 trait SbtEclipseUtils {
 	this: ParentProject =>
+
+	import SbtEclipseUtils._
 
 	/**
 	 * Creates an Eclipse project by name
 	 */
-	lazy val createProject = task { args =>
-		if (args.length == 1) {
-			createEclipseProject(args(0))
-		} else {
-			task {
-				Some("Usage: create-project <project-name>") 
+	lazy val createProject = task {
+		args =>
+			if (args.length == 1) {
+				createEclipseProject(args(0))
+			} else {
+				task {
+					Some("Usage: create-project <project-name>")
+				}
 			}
-		}
-	} describedAs("Creates an Eclipse project by name")
-	
+	} describedAs ("Creates an Eclipse project by name")
+
 	private def createEclipseProject(name: String) = task {
 		val pluginsDir = new File(System.getProperty("user.dir"))
 		val projectDir = new File(pluginsDir, name)
@@ -34,66 +41,68 @@ trait SbtEclipseUtils {
 		} else {
 			projectDir.mkdir()
 
+			val prettyPrinter = new PrettyPrinter(80, 2)
+
 			// create .project
 			val dotProjectXml =
-<projectDescription>
-	<name>{name}</name>
-	<comment></comment>
-	<buildSpec>
-		<buildCommand>
-			<name>org.eclipse.jdt.core.javabuilder</name>
-			<arguments>
-			</arguments>
-		</buildCommand>
-		<buildCommand>
-			<name>org.eclipse.pde.ManifestBuilder</name>
-			<arguments>
-			</arguments>
-		</buildCommand>
-		<buildCommand>
-			<name>org.eclipse.pde.SchemaBuilder</name>
-			<arguments>
-			</arguments>
-		</buildCommand>
-	</buildSpec>
-	<natures>
-		<nature>org.eclipse.jdt.core.javanature</nature>
-		<nature>org.eclipse.pde.PluginNature</nature>
-	</natures>
-</projectDescription>
+				<projectDescription>
+					<name>{name}</name>
+					<comment></comment>
+					<buildSpec>
+						<buildCommand>
+							<name>org.eclipse.jdt.core.javabuilder</name>
+							<arguments>
+							</arguments>
+						</buildCommand>
+						<buildCommand>
+							<name>org.eclipse.pde.ManifestBuilder</name>
+							<arguments>
+							</arguments>
+						</buildCommand>
+						<buildCommand>
+							<name>org.eclipse.pde.SchemaBuilder</name>
+							<arguments>
+							</arguments>
+						</buildCommand>
+					</buildSpec>
+					<natures>
+						<nature>org.eclipse.jdt.core.javanature</nature>
+						<nature>org.eclipse.pde.PluginNature</nature>
+					</natures>
+				</projectDescription>
 
 			writeXml(new File(projectDir, ".project"), dotProjectXml)
 
 			// create .classpath
 			val dotClasspathXml =
-<classpath>
-	<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/J2SE-1.5"/>
-	<classpathentry kind="con" path="org.eclipse.pde.core.requiredPlugins"/>
-	<classpathentry kind="src" path="src"/>
-	<classpathentry kind="output" path="bin"/>
-</classpath>
+				<classpath>
+						<classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/J2SE-1.5"/>
+						<classpathentry kind="con" path="org.eclipse.pde.core.requiredPlugins"/>
+						<classpathentry kind="src" path="src"/>
+						<classpathentry kind="output" path="bin"/>
+				</classpath>
 
-			writeXml(new File(projectDir, ".classpath"), dotClasspathXml )
-			
+			writeXml(new File(projectDir, ".classpath"), dotClasspathXml)
+
 			// create build.properties
 			val buildPropertiesContents = List(
 				"source.. = src/",
 				"bin.includes = META-INF/,\\",
 				"               ."
 			)
-			
+
 			writeLines(new File(projectDir, "build.properties"), buildPropertiesContents)
-			
+
 			// create source directory
 			val sourceDir = new File(projectDir, "src")
 			sourceDir.mkdir()
-			
+
 			// TODO create initial packages
-			
+
 			// create META-INF and MANIFEST.MF
 			val metaInfDir = new File(projectDir, "META-INF")
 			metaInfDir.mkdir()
-			
+
 			val manifestVersion = "Manifest-Version: 1.0"
 			val bundleManifestVersion = "Bundle-ManifestVersion: 2"
 			val bundleName = "Bundle-Name: " + name
@@ -101,7 +110,7 @@ trait SbtEclipseUtils {
 			val bundleVersion = "Bundle-Version: 0.0.0"
 			val bundleRequiredExecEnv = "Bundle-RequiredExecutionEnvironment: J2SE-1.5"
 			val manifestContents = List(manifestVersion, bundleManifestVersion, bundleName, bundleSymbolicName, bundleVersion, bundleRequiredExecEnv, "\n")
-			
+
 			writeLines(new File(metaInfDir, "MANIFEST.MF"), manifestContents)
 
 			// TODO create a test project given a "normal" project
@@ -147,7 +156,7 @@ trait SbtEclipseUtils {
 			val fragmentHostValue = mainAttributes.getValue("Fragment-Host")
 			if (fragmentHostValue != null) {
 				val shortFragmentHostName = shortName(fragmentHostValue)
-				digraph.append( (symbolicName, shortFragmentHostName) )
+				digraph.append((symbolicName, shortFragmentHostName))
 			}
 
 			// record "Require-Bundle" dependencies
@@ -175,14 +184,14 @@ trait SbtEclipseUtils {
 			trimmed
 		}
 	}
-	
+
 	private def writeLines(file: File, lines: List[String]) {
 		FileUtilities.write(file, lines.mkString("\n"), log)
 	}
-	
+
 	private def writeXml(file: File, xml: scala.xml.Elem) {
 		val w = new java.io.StringWriter
-		XML.write(w, xml, "UTF-8", true, null)
-		FileUtilities.write(file, w.toString, log)
+		val formattedXml = PrettyPrinter.format(xml)
+		FileUtilities.write(file, formattedXml, log)
 	}
 }
